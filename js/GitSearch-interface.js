2a8cfde143af8b7357895742858036e75f4dbb12
Joel Waage <joeltwaage@gmail.com>
2016-08-19 12:03:32 -0700

    add ability to search by username, information displays in console

--- js/GitSearch-interface.js
+++ js/GitSearch-interface.js
@@ -1,14 +1,13 @@
 var Git = require('./../js/GitSearch.js').gitModule;
 var input = null;
 
-
 $(document).ready(function() {
   $('.search').submit(function(event) {
     event.preventDefault();
-    var currentGitObject = new Git();
-    currentGitObject.gitSearch();
     input = $('#search-user').val();
     $('#search-user').val("");
+    var currentGitObject = new Git();
+    currentGitObject.gitSearch(input);
     $('.displayResult').text("Displaying");
   });
 });
--- js/GitSearch.js
+++ js/GitSearch.js
@@ -3,20 +3,12 @@ var apiKey = require('./../.env').apiKey;
 function Git() {
 }
 
-Git.prototype.gitSearch = function() {
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+Git.prototype.gitSearch = function(input) {
+  $.get('https://api.github.com/users/' + input + '?access_token=' + apiKey).then(function(response){
     console.log(JSON.stringify(response));
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
-
-// exports.getRepos = function(){
-//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-//     console.log(response);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
 
 exports.gitModule = Git;
