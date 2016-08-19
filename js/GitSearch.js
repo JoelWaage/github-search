b89c0b93563aa9a1010f492f6ac01a279bfe1740
Joel Waage <joeltwaage@gmail.com>
2016-08-19 13:33:53 -0700

    add ability to get index of user's public repositories

--- js/GitSearch.js
+++ js/GitSearch.js
@@ -4,9 +4,10 @@ function Git() {
 }
 
 Git.prototype.gitSearch = function(input) {
-  $.get('https://api.github.com/users/' + input + '?access_token=' + apiKey).then(function(response){
-    $('.displayName').text("Username:  " + response.login);
-    $('.displayUrl').text("User URL:  " + response.url);
+  $.get('https://api.github.com/users/' + input + '/repos?access_token=' + apiKey).then(function(response){
+    // $('.displayName').text("Username:  " + response.login);
+    // $('.displayUrl').text("User URL:  " + response.url);
+    console.log(JSON.stringify(response));
   }).fail(function(error){
     $('.displayName').text("User Not Found");
     console.log(error.responseJSON.message);
