d607c352a420859ed78699fd309b77e6de92717c
Joel Waage <joeltwaage@gmail.com>
2016-08-19 10:15:36 -0700

    add .env file and link apiKey to GitSearch-interface.js

--- js/GitSearch-interface.js
+++ js/GitSearch-interface.js
@@ -1,10 +1,13 @@
 // var Alarm = require('./../js/TEMPLATE.js').templateModule;
+var apiKey = require('./../.env').apiKey
 var input = null;
+
 
 $(document).ready(function() {
   $('.search').submit(function(event) {
     event.preventDefault();
     input = $('#search-user').val();
+    $('#search-user').val("");
     $('.displayResult').text("result text");
   });
 });
