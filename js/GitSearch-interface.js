ce735decfb332e5c69aa88ffb2767a4322de593d
Joel Waage <joeltwaage@gmail.com>
2016-08-19 10:02:17 -0700

    remove installs.txt file from directory

--- js/GitSearch-interface.js
+++ js/GitSearch-interface.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   $('.search').submit(function(event) {
     event.preventDefault();
     input = $('#search-user').val();
-    $('.displayResult').text("this is a big ole testeroonie");
-    console.log(input);
+    $('.displayResult').text("result text");
   });
 });
