7d2e77c0cd337e78695e09a8a2855a8caa4ec9e0
Joel Waage <joeltwaage@gmail.com>
2016-08-19 12:50:41 -0700

    add ability to retrieve user URL, clean up index.html, add error message to page

--- js/GitSearch.js
+++ js/GitSearch.js
@@ -5,8 +5,10 @@ function Git() {
 
 Git.prototype.gitSearch = function(input) {
   $.get('https://api.github.com/users/' + input + '?access_token=' + apiKey).then(function(response){
-    $('.displayResult').text("username: " + response.login);
+    $('.displayName').text("Username:  " + response.login);
+    $('.displayUrl').text("User URL:  " + response.url);
   }).fail(function(error){
+    $('.displayName').text("User Not Found");
     console.log(error.responseJSON.message);
   });
 };
