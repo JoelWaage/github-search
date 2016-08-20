7b9deec90b6e1bfe8ea45914de989b7257500a5b
Joel Waage <joeltwaage@gmail.com>
2016-08-19 23:32:09 -0700

    Adjust spacing in listing div

--- js/GitSearch.js
+++ js/GitSearch.js
@@ -6,8 +6,7 @@ function Git() {
 Git.prototype.gitSearch = function(input) {
   $.get('https://api.github.com/users/' + input + '/repos?access_token=' + apiKey).then(function(response){
     for (var i = 0; i < response.length; i++) {
-      $('.displayName').append("<li><strong>" + response[i].name + "</strong></li><ul><li>" + response[i].description + "</li></ul>");
-      // $('.displayDescription').append("<li>" + response[i].description + "</li>");
+      $('.displayName').append("<li><strong>Repository Name: </strong>" + response[i].name + "</li><ul><li><strong>Description: </strong>" + response[i].description + "</li>" + "<li><strong>URL: </strong>" + response[i].url + "</li></ul><br>");
     }
   }).fail(function(error){
     $('.displayName').text("User Not Found");
