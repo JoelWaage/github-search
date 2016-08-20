8ea533940225ea7c7d31d46b69a3a2be782874dd
Joel Waage <joeltwaage@gmail.com>
2016-08-19 21:21:37 -0700

    add ability to display list of project names and sublist of descriptions

--- js/GitSearch.js
+++ js/GitSearch.js
@@ -5,9 +5,10 @@ function Git() {
 
 Git.prototype.gitSearch = function(input) {
   $.get('https://api.github.com/users/' + input + '/repos?access_token=' + apiKey).then(function(response){
-    // $('.displayName').text("Username:  " + response.login);
-    // $('.displayUrl').text("User URL:  " + response.url);
-    console.log(JSON.stringify(response));
+    for (var i = 0; i < response.length; i++) {
+      $('.displayName').append("<li><strong>" + response[i].name + "</strong></li><ul><li>" + response[i].description + "</li></ul>");
+      // $('.displayDescription').append("<li>" + response[i].description + "</li>");
+    }
   }).fail(function(error){
     $('.displayName').text("User Not Found");
     console.log(error.responseJSON.message);
